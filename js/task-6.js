3d033ede93271be404ca2b399861862f3c14d8a2
Viktoriia Khmel <legchilova.viktoria@gmail.com>
2024-06-28 13:05:18 +0300

    correct task-6

--- js/task-6.js
+++ js/task-6.js
@@ -14,8 +14,6 @@ const box = document.querySelectorAll(".box");
 
 buttonCreate.addEventListener("click", createBoxes);
 
-// buttonCreate.addEventListener("click", handleColor);
-
 buttonCreate.addEventListener("click", clearField);
 
 buttonDelete.addEventListener("click", destroyBoxes);
@@ -23,20 +21,20 @@ buttonDelete.addEventListener("click", destroyBoxes);
 function createBoxes(amount) {
   amount = input.value;
 
-  // const box = `<div class="box"></div>`;
-
   if (amount > 0 && amount <= 100) {
     boxes.classList.add("boxes");
-    for (let i = 0; i < amount; i += 1) {
-      // boxes.innerHTML += box;
-      // boxes.classList.add("boxes");
 
+    let size = 30;
+
+    for (let i = 0; i < amount; i += 1) {
       const box = document.createElement("div");
-      box.style.width = `30 + ${i * 10}px`;
-      box.style.height = `30 + ${i * 10}px`;
+
+      box.style.width = size + "px";
+      box.style.height = size + "px";
+      size += 10;
       box.style.backgroundColor = getRandomHexColor();
 
-      boxes.appendChild(box);
+      boxes.append(box);
     }
   }
 }
@@ -55,9 +53,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-// function handleColor() {
-//   for (let b of box) {
-//     let color = getRandomHexColor();
-//     b.style.backgroundColor = color;
-//   }
-// }
